ffef1b8467c89ae858f0c1e25562a308e7969af9
Sturessonwille <sturessonwille@gmail.com>
2023-09-21 09:34:29 +0200

    ändrade strings, och drawerbase

--- login-auth-test/app/src/main/java/com/example/myapplication/drawerBaseActivity.java
+++ login-auth-test/app/src/main/java/com/example/myapplication/drawerBaseActivity.java
@@ -1,14 +1,54 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class drawerBaseActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class drawerBaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer_base);
+    public void setContentView(View view) {
+
+        drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_drawer_base, null);
+        FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
+        container.addView(view);
+
+        super.setContentView(drawerLayout);
+
+        Toolbar toolbar = drawerLayout.findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = drawerLayout.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.menu_drawer_open, R.string.menu_drawer_closed);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    protected void allocateActivityTitle(String titleString){
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle(titleString);
+        }
+
     }
 }
